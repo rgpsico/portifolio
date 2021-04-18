d73fa9c3229022fd95116df492d09ac37a4dfac5
roger <rogerneves@feegow.com.br>
2021-04-18 01:42:33 -0300

    primeiro commit cms laravel

--- resources/views/Admin/pages/create.blade.php
+++ resources/views/Admin/pages/create.blade.php
@@ -53,6 +53,7 @@
 </div>
 <script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
 
+
 <script>
     tinymce.init({
         selector:'textarea.bodyfield',
