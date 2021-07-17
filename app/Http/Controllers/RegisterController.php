0208df9c205c7cd2351f65efebd0ec4d7e0309c0
rgpsico <gerro121@hotmail.com>
2021-07-16 23:40:45 -0300

    foi

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -7,7 +7,7 @@ use Illuminate\Foundation\Auth\RegisterUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Http\Request;
+use Illuminate\Http\Request;;;
 
 class RegisterController extends Controller
 {
