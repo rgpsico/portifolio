0295fc3a8035a1659e2e4308adf0d8b8edad9211
rgpsico <rgyr2010@hotmail.com>
2022-08-05 01:00:57 -0300

    teste

--- resources/views/Admin/pages/create.blade.php
+++ resources/views/Admin/pages/create.blade.php
@@ -59,7 +59,7 @@
 </div>
 </div>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"  crossorigin="anonymous"></script>
-
+<script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
 
     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
     <script src="{{asset('assets/js/select2.js')}}"></script>
--- resources/views/Admin/pages/edit.blade.php
+++ resources/views/Admin/pages/edit.blade.php
@@ -51,7 +51,7 @@
 </div>
 </div>
 
-
+<script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
 
 <script>
     tinymce.init({
