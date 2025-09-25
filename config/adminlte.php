66402fb5421012d4a547664ac875324151d11633
roger neves <gerro121@hotmail.com>
2025-09-25 10:01:47 -0300

    index html

--- config/adminlte.php
+++ config/adminlte.php
@@ -321,13 +321,13 @@ return [
     */
 
     'filters' => [
-        // JeroenNoten\LaravelAdminLte\Menu\Filters\GateFilter::class,
-        // JeroenNoten\LaravelAdminLte\Menu\Filters\HrefFilter::class,
-        // JeroenNoten\LaravelAdminLte\Menu\Filters\SearchFilter::class,
-        // JeroenNoten\LaravelAdminLte\Menu\Filters\ActiveFilter::class,
-        // JeroenNoten\LaravelAdminLte\Menu\Filters\ClassesFilter::class,
-        // JeroenNoten\LaravelAdminLte\Menu\Filters\LangFilter::class,
-        // JeroenNoten\LaravelAdminLte\Menu\Filters\DataFilter::class,
+        JeroenNoten\LaravelAdminLte\Menu\Filters\GateFilter::class,
+        JeroenNoten\LaravelAdminLte\Menu\Filters\HrefFilter::class,
+        JeroenNoten\LaravelAdminLte\Menu\Filters\SearchFilter::class,
+        JeroenNoten\LaravelAdminLte\Menu\Filters\ActiveFilter::class,
+        JeroenNoten\LaravelAdminLte\Menu\Filters\ClassesFilter::class,
+        JeroenNoten\LaravelAdminLte\Menu\Filters\LangFilter::class,
+        JeroenNoten\LaravelAdminLte\Menu\Filters\DataFilter::class,
     ],
 
     /*
