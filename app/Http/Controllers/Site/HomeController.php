225734d6767634e41d865958c05ac17dadf15d55
roger neves <gerro121@hotmail.com>
2025-09-25 09:30:16 -0300

    index html

--- app/Http/Controllers/Site/HomeController.php
+++ app/Http/Controllers/Site/HomeController.php
@@ -16,7 +16,8 @@ class HomeController extends Controller
     public function index()
     {
         $articles = article::paginate();
-        dd($articles);
+
+
         $portifolio = portifolio::all();
         $users = User::all();
         $experiencia = experiencia::all();
