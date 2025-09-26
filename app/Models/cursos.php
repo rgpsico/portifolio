b641f7a6e4b023cede3d73e57aab5a08851b021d
roger neves <gerro121@hotmail.com>
2025-09-26 13:25:51 -0300

    index html

--- app/Models/cursos.php
+++ app/Models/cursos.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Curso extends Model
+class Cursos extends Model
 {
     use HasFactory;
 
